a640e5d287629dd59c3026de4c67896fba339aaa
손지현_회원/결제개발팀 <hdg12jh@interpark.com>
2024-05-06 15:27:55 +0900

    feat 스택 2개로 큐 구현

---
+++ StackToQueue.java
@@ -0,0 +1,73 @@
+import java.util.Stack;
+
+/**
+ * Stack 2개로 Queue 구현 하기
+ */
+public class StackToQueue {
+    public static class Queue<T> {
+        Stack<T> inStack = new Stack<>();
+        Stack<T> outStack = new Stack<>();
+        T tmp = null;
+
+        // add
+        public T add(T data) {
+            inStack.add(data);
+            return data;
+        }
+
+        // poll
+        public T poll() {
+            outStack.clear();
+            while(inStack.size() > 0) {
+                outStack.add(inStack.pop());
+            }
+
+            tmp = outStack.pop();
+            while(outStack.size() > 0) {
+                inStack.add(outStack.pop());
+            }
+            return tmp;
+        }
+
+        // peek
+        public T peek() {
+            outStack.clear();
+            while(inStack.size() > 0) {
+                outStack.add(inStack.pop());
+            }
+
+            tmp = outStack.peek();
+            while (outStack.size() > 0) {
+                inStack.add(outStack.pop());
+            }
+            return tmp;
+        }
+
+        // clear
+        public void clear() {
+            inStack.clear();
+            outStack.clear();
+        }
+
+        public int size() {
+            return inStack.size();
+        }
+    }
+
+    public static void main(String[] args) {
+        Queue queue = new Queue();
+        queue.add(1);
+        queue.add(2);
+        queue.add(3);
+
+        System.out.println(queue.size()); // 3
+        System.out.println(queue.peek()); // 1
+        System.out.println(queue.poll()); // 1 삭제
+        System.out.println(queue.peek()); // 2
+        queue.add(4); // 2, 3, 4
+        System.out.println(queue.poll()); // 2
+        System.out.println(queue.size()); // 2
+        queue.clear();
+        System.out.println(queue.size()); // 0
+    }
+}
